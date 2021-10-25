34daeea257b8a96aef67d300aad1e3df3150d818
ange1maycry <79104043+ange1maycry@users.noreply.github.com>
2021-10-25 14:46:47 +0300

    Add files via upload

---
+++ academy-php/portfolio/data.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+  'about' => [
+      'name' => 'Иван Иванов',
+      'post' => 'Менеджер',
+      'email' => 'aaa@mail.ru',
+      'phone' => '+79001002030',
+      'site' => 'google.com'
+  ],
+    'education' => [
+        [
+            'faculty' => 'Random faculty',
+            'univercity' => 'Smth Univercity',
+            'yearStart' => 2015,
+            'yearEnd' => 2018
+        ],
+        [
+            'faculty' => 'Random faculty 2',
+            'univercity' => 'Smth Univercity 2',
+            'yearStart' => 2010,
+            'yearEnd' => 2015
+        ],
+    ],
+    'languages' => [
+        'English', 'Russian', 'Spanish'
+    ],
+    'interests' => [
+        'увлечение 1', 'увлечение 2'
+    ],
+    'aboutCareer' => 'lorem lorem lorem',
+
+    'career' => [
+        [
+            'post' => 'post 1',
+            'place' => 'City 1',
+            'duty' => 'lorem lorem 111',
+            'yearStart' => 2016,
+            'yearEnd' => 'настоящее время'
+        ],
+        [
+            'post' => 'post 2',
+            'place' => 'City 2',
+            'duty' => 'lorem lorem 222',
+            'yearStart' => 2010,
+            'yearEnd' => 2015
+        ],
+        [
+            'post' => 'post 3',
+            'place' => 'City 3',
+            'duty' => 'lorem lorem 333',
+            'yearStart' => 2005,
+            'yearEnd' => 2009
+        ],
+    ]
+];
